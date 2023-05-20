c2689f738d03b41e501e1e5a506878f994076174
Andres Vervaecke <andres.vervaecke@ml6.eu>
2023-05-20 13:45:00 +0200

    setup nimbus and cloud function

---
+++ terraform/main/wif.tf
@@ -0,0 +1,8 @@
+locals {
+  # To use wif with Gitlab / Github, please fill in the following local values
+  # and contribute them back to Nimbus.
+  attribute_condition = ""
+  audiences           = [""]
+  issuer_uri          = ""
+}
+
